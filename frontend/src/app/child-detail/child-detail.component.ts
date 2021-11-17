601374c97396c2b355532434d1a974c0a3f3eef4
Vinicius Avansini <vi.avn@hotmail.com>
2021-11-16 22:45:38 -0300

    cruding godParents

--- frontend/src/app/child-detail/child-detail.component.ts
+++ frontend/src/app/child-detail/child-detail.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ChildService } from '../services/child/child.service';
-import { Location } from '@angular/common';
 import { Child, GodParent } from '../services/child/models/Child';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MatTable } from '@angular/material/table';
+import { GodParentsViewModel as GodParentViewModel } from './viewModelds/GodParentsViewModel';
 
 @Component({
   selector: 'app-child-detail',
@@ -14,81 +14,155 @@ import { MatTable } from '@angular/material/table';
 export class ChildDetailComponent implements OnInit {
 
   child: Child | undefined;
-  godParents: GodParent[] = [];
+  godParents: GodParentViewModel[] = [];
   displayedColumns: string[] = ['clothes', 'shoes', 'gift', 'name', 'phone', 'actions'];
+  childFormGroup: FormGroup;
+  godParentsFormGroup: FormGroup;
 
-  formGroup = this.fb.group({
-    child: this.fb.group({
-      id: ['', Validators.required],
-      name: [{ value: '', disabled: true }, Validators.required],
-      age: [{ value: '', disabled: true }, Validators.required],
-      clothesSize: [{ value: '', disabled: true }, Validators.required],
-      shoeSize: [{ value: '', disabled: true }, Validators.required],
-    }),
-
-    family: this.fb.group({
-      id: [{ value: '', disabled: true }, Validators.required],
-      responsible: [{ value: '', disabled: true }, Validators.required],
-      phone: [{ value: '', disabled: true }, Validators.required],
-      address: [{ value: '', disabled: true }, Validators.required],
-    }),
-
-    godParent: this.fb.group({
-      name: ['', Validators.required],
-      phone: ['', Validators.required],
-      clothesSelected: [false],
-      shoeSelected: [false],
-      giftSelected: [false],
-    }),
-  });
+  private readonly addText = 'Adicionar';
+  private readonly updateText = 'Atualizar';
+  godParentsSubmitButtonTitle = this.addText;
 
   @ViewChild(MatTable) table!: MatTable<GodParent>;
 
   constructor(
     private route: ActivatedRoute,
     private childService: ChildService,
-    private location: Location,
     private fb: FormBuilder,
-  ) { }
+  ) {
+    this.childFormGroup = this.createChildFormGroup();
+    this.godParentsFormGroup = this.createGodParentsFormGroup();
+  }
 
   ngOnInit(): void {
     this.childService.getChild(this.route.snapshot.paramMap.get('id')!)
       .subscribe(child => {
         this.child = child;
         if (child) {
-          this.formGroup.patchValue({
-            child: {
-              id: child.id,
-              name: child.name,
-              age: child.age,
-              clothesSize: child.clothesSize,
-              shoeSize: child.shoeSize,
-            },
-            family: {
-              id: child.familyId,
-              responsible: child.responsible,
-              phone: child.phone,
-              address: child.address,
-            }
-          });
+          this.setValuesToChildForm(child);
         }
       });
   }
 
-  onSubmit(): void {
-    const godParent = this.formGroup.value.godParent;
+  onSubmit(event: any): void {
+    const godParentFormValues = this.godParentsFormGroup.value;
+    if (godParentFormValues.rowId) {
+      const godParentIndex = this.godParents.findIndex(gp => gp.rowId === godParentFormValues.rowId)
+      if (godParentIndex >= 0) {
+        this.godParents[godParentIndex] = {
+          ...this.godParents[godParentIndex],
+          godParent: {
+            ...this.godParents[godParentIndex].godParent,
+            name: godParentFormValues.name,
+            phone: godParentFormValues.phone,
+            isGiftSelected: godParentFormValues.giftSelected,
+            isClothesSelected: godParentFormValues.clothesSelected,
+            isShoeSelected: godParentFormValues.shoeSelected,
+          }
+        };
+      }
 
-    this.godParents.push({
-      id: '',
-      name: godParent.name,
-      phone: godParent.phone,
-      isGiftSelected: godParent.giftSelected,
-      isClothesSelected: godParent.clothesSelected,
-      isShoeSelected: godParent.shoeSelected,
-      childId: this.formGroup.value.child.id,
-    });
+    } else {
+      const sortedGodParents = this.godParents.sort((a, b) => a.createdDate.getTime() - b.createdDate.getTime());
+      let lastId = 0;
+
+      if (sortedGodParents.length > 0) {
+        lastId = sortedGodParents[sortedGodParents.length - 1].rowId
+      }
+
+      this.godParents.push({
+        rowId: lastId + 1,
+        createdDate: new Date(),
+        godParent: {
+          id: '',
+          name: godParentFormValues.name,
+          phone: godParentFormValues.phone,
+          isGiftSelected: godParentFormValues.giftSelected,
+          isClothesSelected: godParentFormValues.clothesSelected,
+          isShoeSelected: godParentFormValues.shoeSelected,
+          childId: this.childFormGroup.value.child.id,
+        }
+      });
+    }
 
     this.table.renderRows();
-    this.formGroup.get('godParent')!.reset();
+    event.currentTarget.reset();
+    this.godParentsFormGroup.reset();
+    this.resetSubmitButtonText();
+  }
+
+  cancelGodParentAction() {
+    this.godParentsFormGroup.reset();
+    this.resetSubmitButtonText();
+  }
+
+  deleteGodParentFromTable(godParent: GodParentViewModel): void {
+    this.godParents = this.godParents.filter(gp => gp.rowId !== godParent.rowId);
+    this.table.renderRows();
+  }
+
+  onRowClicked(row: GodParentViewModel): void {
+    this.godParentsSubmitButtonTitle = this.updateText;
+
+    this.godParentsFormGroup.patchValue({
+      rowId: row.rowId,
+      name: row.godParent.name,
+      phone: row.godParent.phone,
+      clothesSelected: row.godParent.isClothesSelected,
+      shoeSelected: row.godParent.isShoeSelected,
+      giftSelected: row.godParent.isGiftSelected,
+    });
+  }
+
+  private createChildFormGroup(): FormGroup {
+    return this.fb.group({
+      child: this.fb.group({
+        id: [null, Validators.required],
+        name: [{ value: null, disabled: true }, Validators.required],
+        age: [{ value: null, disabled: true }, Validators.required],
+        clothesSize: [{ value: null, disabled: true }, Validators.required],
+        shoeSize: [{ value: null, disabled: true }, Validators.required],
+      }),
+
+      family: this.fb.group({
+        id: [{ value: null, disabled: true }, Validators.required],
+        responsible: [{ value: null, disabled: true }, Validators.required],
+        phone: [{ value: null, disabled: true }, Validators.required],
+        address: [{ value: null, disabled: true }, Validators.required],
+      }),
+    });
+  }
+
+  private resetSubmitButtonText(): void {
+    this.godParentsSubmitButtonTitle = this.addText;
+  }
+
+  private createGodParentsFormGroup(): FormGroup {
+    return this.fb.group({
+      rowId: [null],
+      name: [null, Validators.required],
+      phone: [null, Validators.required],
+      clothesSelected: [false],
+      shoeSelected: [false],
+      giftSelected: [false],
+    });
+  }
+
+  private setValuesToChildForm(child: Child): void {
+    this.childFormGroup.patchValue({
+      child: {
+        id: child.id,
+        name: child.name,
+        age: child.age,
+        clothesSize: child.clothesSize,
+        shoeSize: child.shoeSize,
+      },
+      family: {
+        id: child.familyId,
+        responsible: child.responsible,
+        phone: child.phone,
+        address: child.address,
+      },
+    });
   }
 }
---
+++ frontend/src/app/child-detail/viewModelds/GodParentsViewModel.ts
@@ -0,0 +1,7 @@
+import { GodParent } from "src/app/services/child/models/Child";
+
+export interface GodParentsViewModel {
+  rowId: number;
+  createdDate: Date;
+  godParent: GodParent;
+}
