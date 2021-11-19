3d01a4d3ae147480240f3f605a3d684b75496702
Vinicius Avansini <vi.avn@hotmail.com>
2021-11-18 23:10:57 -0300

    developing backend

---
+++ backend/Jcc.Api/JccApi/Controllers/HealthCheckController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace JccApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        private readonly ILogger<HealthCheckController> _logger;
+
+        public HealthCheckController(ILogger<HealthCheckController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Helthcheck endpoint called");
+            return Ok();
+        }
+    }
+}
