9d9cfadec529fd1e6689dbaf75347be095f2b981
Vinicius Avansini <vi.avn@hotmail.com>
2021-11-22 23:16:37 -0300

    adding Dockerfile

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AuthGuard } from './guards/auth/auth.guard';
 import { ChildDetailComponent } from './pages/child-detail/child-detail.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './pages/login/login.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'home', component: DashboardComponent, canActivate: [AuthGuard] },
-  { path: 'detail/:id', component: ChildDetailComponent, canActivate: [AuthGuard] },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: DashboardComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'detail/:id', component: ChildDetailComponent, canActivate: [AuthGuard] },
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
